7fed1c0cad031cffee7d3c76d8733ed259c4655f
Clark Gaebel <cgaebel@mozilla.com>
2014-09-20 18:40:23 -0700

    Added IORingbufs.

--- src/lib.rs
+++ src/lib.rs
@@ -1996,6 +1996,59 @@ impl<'a> Show for RWIobuf<'a> {
   }
 }
 
+/// A ring buffer implemented with `Iobuf`s.
+pub struct IORingbuf {
+  /// The contents of the window is space for input to be put into. Therefore,
+  /// initially full.
+  i_buf: RWIobuf<'static>,
+  /// The contents of the window are things needing to be output. Therefore,
+  /// initally empty.
+  o_buf: RWIobuf<'static>,
+}
+
+impl IORingbuf {
+  /// Creates a new ring buffer, with room for `cap` bytes.
+  pub fn new(cap: uint) -> IORingbuf {
+    let left_size = cap / 2;
+    let mut ret =
+      IORingbuf {
+        i_buf: RWIobuf::new(left_size),
+        o_buf: RWIobuf::new(cap - left_size),
+      };
+    ret.o_buf.flip_lo(); // start with an empty o_buf.
+    ret
+  }
+
+  /// Returns an Iobuf, whose window may be filled with new data. This acts as
+  /// the "push" operations for the ringbuf.
+  pub fn push_buf(&mut self) -> &mut RWIobuf<'static> {
+    &mut self.i_buf
+  }
+
+  /// Returns an Iobuf, whose window may be have data `consume`d out of it. This
+  /// acts as the "pop" operation for the ringbuf.
+  pub fn pop_buf(&mut self) -> &mut ROIobuf<'static> {
+    if self.o_buf.is_empty() {
+      self.i_buf.flip_lo();
+      self.o_buf.reset();
+      mem::swap(&mut self.i_buf, &mut self.o_buf);
+    }
+    // Clients should only be doing read-only operations into the iobuf, so
+    // return a ROIobuf.
+    unsafe { mem::transmute(&mut self.o_buf) }
+  }
+
+  /// `true` if there is no data to pop in the Iobuf.
+  pub fn is_empty(&self) -> bool {
+    self.i_buf.cap() == self.i_buf.len() && self.o_buf.is_empty()
+  }
+
+  /// `true` if there is no room for new data in the Iobuf.
+  pub fn is_full(&self) -> bool {
+    self.i_buf.is_empty()
+  }
+}
+
 #[test]
 fn peek_be() {
   let s = [1,2,3,4];
