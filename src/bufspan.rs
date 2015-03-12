94a34ff6854bd2280a9247caf03e7ca3f7e3766d
Clark Gaebel <cg.wowus.cg@gmail.com>
2015-03-12 19:04:46 -0400

    add lots of bufspan tests and optimizations

--- src/bufspan.rs
+++ src/bufspan.rs
@@ -1,9 +1,8 @@
 use std::cmp::Ordering;
 use std::fmt::{self, Formatter, Debug};
-use std::intrinsics::move_val_init;
+use std::intrinsics::{assume, move_val_init};
 use std::iter::{self, order, IntoIterator, FromIterator, AdditiveIterator};
 use std::mem;
-use std::num::ToPrimitive;
 use std::option;
 use std::slice;
 use std::vec;
@@ -13,6 +12,305 @@ use iobuf::Iobuf;
 use BufSpan::{Empty, One, Many};
 use SpanIter::{Opt, Lot};
 use SpanMoveIter::{MoveOpt, MoveLot};
+
+#[cold]
+fn bytes_in_vbuf<Buf: Iobuf>(v: &[Buf]) -> usize {
+  v.into_iter().map(|b| b.len() as usize).sum()
+}
+
+#[test]
+fn test_bytes_in_vbuf() {
+  use impls::ROIobuf;
+
+  let bs = [
+    ROIobuf::from_str("1234"),
+    ROIobuf::from_str("5678"),
+    ROIobuf::from_str("9"),
+  ];
+
+  assert_eq!(bytes_in_vbuf(&bs), 9);
+  assert_eq!(bytes_in_vbuf(&bs[1..]), 5);
+  assert_eq!(bytes_in_vbuf(&bs[2..]), 1);
+}
+
+#[cold]
+fn count_bytes_cmp_vbuf<Buf: Iobuf>(v: &[Buf], mut other: usize) -> Ordering {
+  for b in v {
+    let len = b.len() as usize;
+    if len > other { return Ordering::Greater }
+    other -= len;
+  }
+  if other == 0 { Ordering::Equal }
+  else          { Ordering::Less  }
+}
+
+#[test]
+fn test_count_bytes_cmp_vbuf() {
+  use impls::ROIobuf;
+
+  let bs = [
+    ROIobuf::from_str("1234"),
+    ROIobuf::from_str("5678"),
+    ROIobuf::from_str("9"),
+  ];
+
+  assert_eq!(count_bytes_cmp_vbuf(&bs, 0 ), Ordering::Greater);
+  assert_eq!(count_bytes_cmp_vbuf(&bs, 10), Ordering::Less);
+  assert_eq!(count_bytes_cmp_vbuf(&bs, 9 ), Ordering::Equal);
+}
+
+#[cold]
+fn byte_equal_slice_vbuf<Buf: Iobuf>(v: &[Buf], mut other: &[u8]) -> bool {
+  if count_bytes_cmp_vbuf(v, other.len()) != Ordering::Equal {
+    return false;
+  }
+
+  unsafe {
+    for b in v {
+      let b_as_slice = b.as_window_slice();
+      assume(other.len() >= b_as_slice.len());
+      let (start, new_other) = other.split_at(b_as_slice.len());
+      if b_as_slice != start { return false; }
+      other = new_other;
+    }
+  }
+
+  true
+}
+
+#[test]
+fn test_byte_equal_slice_vbuf() {
+  use impls::ROIobuf;
+
+  let bs = [
+    ROIobuf::from_str("1234"),
+    ROIobuf::from_str("5678"),
+    ROIobuf::from_str("9"),
+  ];
+
+  assert!(byte_equal_slice_vbuf(&bs, b"123456789"));
+  assert!(!byte_equal_slice_vbuf(&bs, b"123456780"));
+  assert!(!byte_equal_slice_vbuf(&bs, b"987654321"));
+  assert!(!byte_equal_slice_vbuf(&bs, b"12345678"));
+  assert!(!byte_equal_slice_vbuf(&bs, b"23456789"));
+}
+
+#[inline]
+fn byte_equal_buf_buf<Buf1: Iobuf, Buf2: Iobuf>(x: &Buf1, y: &Buf2) -> bool {
+  unsafe {
+    x.as_window_slice() == y.as_window_slice()
+  }
+}
+
+#[inline]
+fn byte_equal_buf_vbuf<Buf1: Iobuf, Buf2: Iobuf>(x: &Buf1, y: &[Buf2]) -> bool {
+  unsafe { byte_equal_slice_vbuf(y, x.as_window_slice()) }
+}
+
+#[cold]
+fn byte_equal_vbuf_vbuf<Buf1: Iobuf, Buf2: Iobuf>(x: &[Buf1], y: &[Buf2]) -> bool {
+  if count_bytes_cmp_vbuf(x, bytes_in_vbuf(y)) != Ordering::Equal {
+    return false;
+  }
+
+  unsafe {
+    x.into_iter().flat_map(|b| b.as_window_slice().into_iter())
+    .zip(y.iter().flat_map(|b| b.as_window_slice().into_iter()))
+    .all(|(x, y)| x == y)
+  }
+}
+
+#[test]
+fn test_byte_equal_vbuf_vbuf() {
+  use impls::ROIobuf;
+
+  let b0 = [
+    ROIobuf::from_str("1234"),
+    ROIobuf::from_str("5678"),
+    ROIobuf::from_str("9"),
+  ];
+
+  let b1 = [
+    ROIobuf::from_str("12"),
+    ROIobuf::from_str("34567"),
+    ROIobuf::from_str("89"),
+  ];
+
+  let b2 = [
+    ROIobuf::from_str("123456789"),
+  ];
+
+  let b3 = [
+    ROIobuf::from_str("123456780"),
+  ];
+
+  let b4 = [
+    ROIobuf::from_str("11111111111111"),
+  ];
+
+  assert!(byte_equal_vbuf_vbuf(&b0, &b1));
+  assert!(byte_equal_vbuf_vbuf(&b1, &b0));
+
+  assert!(byte_equal_vbuf_vbuf(&b0, &b2));
+  assert!(byte_equal_vbuf_vbuf(&b2, &b0));
+
+  assert!(byte_equal_vbuf_vbuf(&b1, &b2));
+  assert!(byte_equal_vbuf_vbuf(&b2, &b1));
+
+  assert!(!byte_equal_vbuf_vbuf(&b0, &b3));
+  assert!(!byte_equal_vbuf_vbuf(&b1, &b3));
+  assert!(!byte_equal_vbuf_vbuf(&b2, &b3));
+
+  assert!(!byte_equal_vbuf_vbuf(&b0, &b4));
+  assert!(!byte_equal_vbuf_vbuf(&b1, &b4));
+  assert!(!byte_equal_vbuf_vbuf(&b2, &b4));
+
+  assert!(!byte_equal_vbuf_vbuf(&b3, &b4));
+}
+
+/// `true` if `v` starts with `other`
+#[cold]
+fn starts_with_vbuf<Buf: Iobuf>(v: &[Buf], mut other: &[u8]) -> bool {
+  for b in v {
+    let b = unsafe { b.as_window_slice() };
+    match b.len().cmp(&other.len()) {
+      Ordering::Greater => return b.starts_with(other),
+      Ordering::Equal   => return b == other,
+      Ordering::Less    => {
+        let (start, new_other) = other.split_at(b.len());
+        if b != start { return false; }
+        other = new_other;
+      }
+    }
+  }
+
+  // Walked through all of `v`. If `other` is empty, `v` == `other`, and
+  // therefore, `v` starts with `other`.
+  other.is_empty()
+}
+
+#[test]
+fn test_starts_with_vbuf() {
+  use impls::ROIobuf;
+
+  let b0 = [
+    ROIobuf::from_str("1234"),
+    ROIobuf::from_str("5678"),
+    ROIobuf::from_str("9"),
+  ];
+
+  assert!(starts_with_vbuf(&b0, b"123456789"));
+  assert!(starts_with_vbuf(&b0, b"12345678"));
+  assert!(starts_with_vbuf(&b0, b""));
+  assert!(starts_with_vbuf(&b0, b"12345"));
+
+  assert!(!starts_with_vbuf(&b0, b"123456780"));
+  assert!(!starts_with_vbuf(&b0, b"1234567890"));
+  assert!(!starts_with_vbuf(&b0, b"123450789"));
+
+  assert!(!starts_with_vbuf(&b0, b"2"));
+}
+
+#[cold]
+fn ends_with_vbuf<Buf: Iobuf>(v: &[Buf], mut other: &[u8]) -> bool {
+  for b in v.into_iter().rev() {
+    let b = unsafe { b.as_window_slice() };
+    match b.len().cmp(&other.len()) {
+      Ordering::Greater => return b.ends_with(other),
+      Ordering::Equal   => return b == other,
+      Ordering::Less    => {
+        let (new_other, end) = other.split_at(other.len() - b.len());
+        if b != end { return false; }
+        other = new_other;
+      }
+    }
+  }
+
+  // Walked through all of `v`. If `other` is empty, `v` == `other`, and
+  // therefore, `v` ends with `other`.
+  other.is_empty()
+}
+
+#[test]
+fn test_ends_with_vbuf() {
+  use impls::ROIobuf;
+
+  let b0 = [
+    ROIobuf::from_str("1234"),
+    ROIobuf::from_str("5678"),
+    ROIobuf::from_str("9"),
+  ];
+
+  assert!(ends_with_vbuf(&b0, b"123456789"));
+  assert!(ends_with_vbuf(&b0, b"23456789"));
+  assert!(ends_with_vbuf(&b0, b"3456789"));
+  assert!(ends_with_vbuf(&b0, b"456789"));
+  assert!(ends_with_vbuf(&b0, b"56789"));
+  assert!(ends_with_vbuf(&b0, b"6789"));
+  assert!(ends_with_vbuf(&b0, b"9"));
+  assert!(ends_with_vbuf(&b0, b""));
+
+  assert!(!ends_with_vbuf(&b0, b"1234567890"));
+  assert!(!ends_with_vbuf(&b0, b"023456789"));
+  assert!(!ends_with_vbuf(&b0, b"123456780"));
+  assert!(!ends_with_vbuf(&b0, b"123450789"));
+  assert!(!ends_with_vbuf(&b0, b"987654321"));
+}
+
+#[inline]
+fn cmp_buf_buf<Buf: Iobuf>(bx: &Buf, by: &Buf) -> Ordering {
+  unsafe {
+    order::cmp(
+      bx.as_window_slice().into_iter(),
+      by.as_window_slice().into_iter())
+  }
+}
+
+#[cold]
+fn cmp_buf_vec<Buf: Iobuf>(b: &Buf, v: &[Buf]) -> Ordering {
+  let mut b = unsafe { b.as_window_slice() };
+
+  for x in v {
+    let x = unsafe { x.as_window_slice() };
+
+    if b.len() >= x.len() as usize {
+      let (start, new_b) = b.split_at(x.len() as usize);
+
+      match order::cmp(start.into_iter(), x.into_iter()) {
+        Ordering::Equal => { b = new_b; }
+        order => return order,
+      }
+    } else {
+      return order::cmp((&b).into_iter(), x.into_iter());
+    }
+  }
+
+  if b.is_empty() { Ordering::Equal } else { Ordering::Greater }
+}
+
+#[test]
+fn test_cmp_buf_vec() {
+  use impls::ROIobuf;
+
+  let b0 = [
+    ROIobuf::from_str("1234"),
+    ROIobuf::from_str("5678"),
+    ROIobuf::from_str("9"),
+  ];
+
+  assert_eq!(cmp_buf_vec(&ROIobuf::from_str("123456789"), &b0), Ordering::Equal);
+
+  assert_eq!(cmp_buf_vec(&ROIobuf::from_str("12345678"), &b0), Ordering::Less);
+  assert_eq!(cmp_buf_vec(&ROIobuf::from_str("1234567890"), &b0), Ordering::Greater);
+
+  assert_eq!(cmp_buf_vec(&ROIobuf::from_str("023456789"), &b0), Ordering::Less);
+  assert_eq!(cmp_buf_vec(&ROIobuf::from_str("223456789"), &b0), Ordering::Greater);
+}
+
+#[cold]
+fn cmp_vec_vec<Buf: Iobuf>(vx: &BufSpan<Buf>, vy: &BufSpan<Buf>) -> Ordering {
+  order::cmp(vx.iter_bytes(), vy.iter_bytes())
+}
 
 /// A span over potentially many Iobufs. This is useful as a "string" type where
 /// the contents of the string can come from multiple IObufs, and you want to
@@ -26,6 +324,7 @@ use SpanMoveIter::{MoveOpt, MoveLot};
 ///
 /// A `BufSpan` is internally represented as either an `Iobuf` or a `Vec<Iobuf>`,
 /// depending on how many different buffers were used.
+#[derive(Clone)]
 pub enum BufSpan<Buf> {
   /// A span over 0 bytes.
   Empty,
@@ -33,17 +332,6 @@ pub enum BufSpan<Buf> {
   One (Buf),
   /// A span over several backing Iobufs.
   Many(Vec<Buf>),
-}
-
-impl<Buf: Iobuf> Clone for BufSpan<Buf> {
-  #[inline]
-  fn clone(&self) -> BufSpan<Buf> {
-    match *self {
-      Empty       => Empty,
-      One(ref b)  => One ((*b).clone()),
-      Many(ref v) => Many((*v).clone()),
-    }
-  }
 }
 
 impl<Buf: Iobuf> Debug for BufSpan<Buf> {
@@ -304,8 +592,15 @@ impl<Buf: Iobuf> BufSpan<Buf> {
   /// ```
   #[inline]
   pub fn byte_equal<Buf2: Iobuf>(&self, other: &BufSpan<Buf2>) -> bool {
-    self.count_bytes_cmp(other.count_bytes()) == Ordering::Equal
-    && self.iter_bytes().zip(other.iter_bytes()).all(|(a, b)| a == b)
+    match (self, other) {
+      (&Empty      , &Empty      ) => true,
+      (&Empty      ,     _       ) => false,
+      (    _       , &Empty      ) => false,
+      (&One (ref x), &One (ref y)) => byte_equal_buf_buf(x, y),
+      (&One (ref x), &Many(ref y)) => byte_equal_buf_vbuf(x, y),
+      (&Many(ref x), &One (ref y)) => byte_equal_buf_vbuf(y, x),
+      (&Many(ref x), &Many(ref y)) => byte_equal_vbuf_vbuf(x, y),
+    }
   }
 
   /// A more efficient version of byte_equal, specialized to work exclusively on
@@ -321,14 +616,11 @@ impl<Buf: Iobuf> BufSpan<Buf> {
   /// ```
   #[inline]
   pub fn byte_equal_slice(&self, other: &[u8]) -> bool {
-    let other_len =
-      match other.len().to_u32() {
-        None        => return false,
-        Some(other) => other,
-      };
-
-    self.count_bytes_cmp(other_len) == Ordering::Equal
-    && self.iter_bytes().zip(other.iter()).all(|(a, &b)| a == b)
+    match *self {
+      Empty       => other.is_empty(),
+      One (ref b) => unsafe { b.as_window_slice() == other },
+      Many(ref v) => byte_equal_slice_vbuf(v, other),
+    }
   }
 
   /// Counts the number of bytes this `BufSpan` is over. This is
@@ -344,14 +636,13 @@ impl<Buf: Iobuf> BufSpan<Buf> {
   /// assert_eq!(a.count_bytes(), 11); // iterates over the pushed buffers.
   /// ```
   #[inline]
-  pub fn count_bytes(&self) -> u32 {
-
+  pub fn count_bytes(&self) -> usize {
     // `self.into_iter().map(|b| b.len()).sum()` would be shorter, but I like to
     // specialize for the much more common case of empty or singular `BufSpan`s.
     match *self {
       Empty       => 0,
-      One (ref b) => b.len(),
-      Many(ref v) => v.iter().map(|b| b.len()).sum(),
+      One (ref b) => b.len() as usize,
+      Many(ref v) => bytes_in_vbuf(v),
     }
   }
 
@@ -373,19 +664,11 @@ impl<Buf: Iobuf> BufSpan<Buf> {
   /// assert_eq!(a.count_bytes_cmp(9001), Ordering::Less);
   /// ```
   #[inline]
-  pub fn count_bytes_cmp(&self, mut other: u32) -> Ordering {
+  pub fn count_bytes_cmp(&self, other: usize) -> Ordering {
     match *self {
       Empty       => 0.cmp(&other),
-      One (ref b) => b.len().cmp(&other),
-      Many(ref v) => {
-        for b in v {
-          let len = b.len();
-          if len > other { return Ordering::Greater }
-          other -= len;
-        }
-        if other == 0 { Ordering::Equal }
-        else          { Ordering::Less  }
-      }
+      One (ref b) => (b.len() as usize).cmp(&other),
+      Many(ref v) => count_bytes_cmp_vbuf(v, other),
     }
   }
 
@@ -406,7 +689,7 @@ impl<Buf: Iobuf> BufSpan<Buf> {
   #[inline]
   pub fn append(&mut self, other: Self) {
     if self.is_empty() {
-      *self = other;
+      unsafe { move_val_init(self, other) }
     } else {
       self.extend(other.into_iter())
     }
@@ -431,14 +714,11 @@ impl<Buf: Iobuf> BufSpan<Buf> {
   /// ```
   #[inline]
   pub fn starts_with(&self, other: &[u8]) -> bool {
-    let other_len =
-      match other.len().to_u32() {
-        None        => return false,
-        Some(other) => other,
-      };
-
-    if self.count_bytes_cmp(other_len) == Ordering::Less { return false }
-    self.iter_bytes().zip(other.iter()).all(|(a, b)| a == *b)
+    match *self {
+      Empty => other.is_empty(),
+      One(ref b) => unsafe { b.as_window_slice().starts_with(other) },
+      Many(ref v) => starts_with_vbuf(v, other),
+    }
   }
 
   /// Returns `true` if the span ends with the given bytes.
@@ -460,18 +740,16 @@ impl<Buf: Iobuf> BufSpan<Buf> {
   /// ```
   #[inline]
   pub fn ends_with(&self, other: &[u8]) -> bool {
-    let other_len =
-      match other.len().to_u32() {
-        None        => return false,
-        Some(other) => other,
-      };
-
-    if self.count_bytes_cmp(other_len) == Ordering::Less { return false }
-    self.iter_bytes().rev().zip(other.iter().rev()).all(|(a, b)| a == *b)
+    match *self {
+      Empty       => other.is_empty(),
+      One (ref b) => unsafe { b.as_window_slice().ends_with(other) },
+      Many(ref v) => ends_with_vbuf(v, other),
+    }
   }
 }
 
 impl<Buf: Iobuf> PartialEq for BufSpan<Buf> {
+  #[inline]
   fn eq(&self, other: &Self) -> bool {
     self.byte_equal(other)
   }
@@ -480,14 +758,24 @@ impl<Buf: Iobuf> PartialEq for BufSpan<Buf> {
 impl<Buf: Iobuf> Eq for BufSpan<Buf> {}
 
 impl<Buf: Iobuf> PartialOrd for BufSpan<Buf> {
+  #[inline]
   fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-    order::partial_cmp(self.iter_bytes(), other.iter_bytes())
+    Some(self.cmp(other))
   }
 }
 
 impl<Buf: Iobuf> Ord for BufSpan<Buf> {
+  #[inline]
   fn cmp(&self, other: &Self) -> Ordering {
-    order::cmp(self.iter_bytes(), other.iter_bytes())
+    match (self, other) {
+      (&Empty, &Empty) => Ordering::Equal,
+      (&Empty,    _  ) => Ordering::Less,
+      (  _   , &Empty) => Ordering::Greater,
+      (&One (ref bx), &One (ref by)) => cmp_buf_buf(bx, by),
+      (&One (ref bx), &Many(ref vy)) => cmp_buf_vec(bx, vy),
+      (&Many(ref vx), &One (ref by)) => cmp_buf_vec(by, vx).reverse(),
+      (&Many(   _  ), &Many(   _  )) => cmp_vec_vec(self, other),
+    }
   }
 }
 
